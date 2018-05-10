179c0ff23e86cb09df5fc2ae0a3625674d538ace
forxtu <forxtu@gmail.com>
2018-05-10 23:39:27 +0200

    chore(icons): change

--- src/scripts/icons.js
+++ src/scripts/icons.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 
 // font awesome icons
-import 'vue-awesome/icons/star'
-import 'vue-awesome/icons/search'
-import 'vue-awesome/icons/chevron-right'
-import 'vue-awesome/icons/tag'
-// import 'vue-awesome/icons/facebook-square'
-// import 'vue-awesome/icons/twitter'
-import Icon from 'vue-awesome/components/Icon'
+import 'vue-awesome/icons/star';
+import 'vue-awesome/icons/search';
+import 'vue-awesome/icons/chevron-right';
+import 'vue-awesome/icons/chevron-left';
+import 'vue-awesome/icons/tag';
+// import 'vue-awesome/icons/facebook-square';
+// import 'vue-awesome/icons/twitter';
+import Icon from 'vue-awesome/components/Icon';
 
-export default Vue.component('icon', Icon)
+export default Vue.component('icon', Icon);
